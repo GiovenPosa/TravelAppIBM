31e42be1c41c809e4cff2b739f99467fca012239
ShabarishMenon <shabarishmenon11@gmail.com>
2025-06-17 20:13:51 +0100

    Delete backend directory

--- backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-const { authenticateToken } = require('../middleware/authMiddleware');
-
-router.post('/', authenticateToken, chatController.getOrCreateChat);
-
-module.exports = router;
--- backend/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middleware/authMiddleware');
-const searchController = require('../controllers/searchController');
-
-router.get('/', authenticateToken, searchController.search);
-
-module.exports = router;
